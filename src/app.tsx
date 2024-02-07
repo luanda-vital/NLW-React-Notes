3455bd5e7ad2c11063805055093bb61f7894ee59
Luanda Vital <luavital0@gmail.com>
2024-02-07 15:17:34 -0300

    aula 02

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
 
+const note = {
+  date: new Date(),
+  content: "kjhsdfk kasjhkjashfkas fkjdshfjksdhfjks fkjsadhfjkshdfa hgdjsgdsfgs"
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -20,9 +25,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note}/>
       </div>
     </div>
   )
